04914b5cb85662e5d34e1843752bd6cc36183303
Nikikapralov <nikikapralov1@gmail.com>
2025-01-24 01:04:39 +0100

    added daily leetcode

---
+++ Algorithmic_problems/Leetcode/01_Two_sum/main.py
@@ -0,0 +1,28 @@
+def two_sum(nums: list[int], target: int) -> list[int]:
+    """
+    Given an array of integers nums and an integer target,
+    return indices of the two numbers such that they add up to target.
+
+    My solution:
+    For each number in numbers: O(N)
+        Find 9 - number and save it in map with index. (O1)
+        If number in map: (O1)
+            return current_index and number_index. (O1)
+
+    :param nums: The input array.
+    :param target: The number to sum up to.
+    :return: A list with the indexes of the two numbers that sum up to the target.
+    """
+    number_index_map: dict[int, int] = {}
+
+    for index, value in enumerate(nums):
+        number: int = target - value
+        if number in number_index_map:
+            result: list[int] = [number_index_map[number], index]
+            return result
+        number_index_map[value] = index
+
+
+print(two_sum(nums=[2, 7, 11, 15], target=9))
+print(two_sum(nums=[3, 2, 4], target=6))
+print(two_sum(nums=[3, 3], target=6))
---
+++ Algorithmic_problems/Leetcode/11_Container_with_most_water/main.py
@@ -0,0 +1,55 @@
+def container_with_most_water(height: list[int]) -> int:
+    """
+    You are given an integer array height of length n.
+    There are n vertical lines drawn such that the two endpoints of the ith line are (i, 0) and (i, height[i]).
+    Find two lines that together with the x-axis form a container, such that the container contains the most water.
+    Return the maximum amount of water a container can store.
+
+    Solution:
+    Area = a * b
+    I set a pointer to the right and to the left of the array.
+    I calculate the area between the 2 columns.
+    I move the smaller pointer one position.
+    I calculate the area again.
+    Repeat until the biggest area is found.
+
+    This works because with this approach, we are limited by the height of the smallest wall. (Water overflow)
+    and we subsequently squeeze the container tighter and tighter until we reach out of options.
+
+    For each column:
+
+    :param height: An array of columns, where each integer value is the height of the column.
+    :return: The maximum area of the water the container can hold.
+    """
+    area: int = 0
+    right_pointer: int = len(height) - 1
+    left_pointer: int = 0
+
+    while right_pointer >= 0 and left_pointer <= len(height) - 1:
+        right: int = height[right_pointer]
+        left: int = height[left_pointer]
+        distance: int = right_pointer - left_pointer
+
+        if right > left:
+            smallest = left
+            left_pointer += 1
+        else:
+            smallest = right
+            right_pointer -= 1
+
+        new_area = distance * smallest
+
+        if new_area > area:
+            area = new_area
+
+    return area
+
+
+
+
+
+
+
+
+print(container_with_most_water(height=[1, 8, 6, 2, 5, 4, 8, 3, 7]))
+print(container_with_most_water(height=[1, 1]))
